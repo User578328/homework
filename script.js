4e6d0d551259b46303155c9d986b3ee15de5a4c5
User578328 <129718685+User578328@users.noreply.github.com>
2023-04-03 07:31:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const users = [
+    {username: 'user1', password: 'password1'},
+    {username: 'user2', password: 'password2'},
+    {username: 'user3', password: 'password3'}
+  ];
+  
+  const loginBtn = document.getElementById('login-btn');
+  
+  loginBtn.addEventListener('click', function() {
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    
+    let isLoggedIn = false;
+    
+    for (let i = 0; i < users.length; i++) {
+      if (username === users[i].username && password === users[i].password) {
+        isLoggedIn = true;
+        break;
+      }
+    }
+    
+    if (isLoggedIn) {
+      alert('Úspěšně přihlášen.');
+      // přesměrování na další stránku, když je úspěšně přihl
+  
